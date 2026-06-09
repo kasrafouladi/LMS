b92594d4aad76ab6b6e10c58bbd2d2e5b3e0015e
Setayesh yarijani <152412073+setayeshyarijani@users.noreply.github.com>
2026-06-09 15:25:39 +0330

    .

--- online_school_backend/app/main.py
+++ online_school_backend/app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
-from app.routers import auth, users, courses, enrollments, assignments, attendance, certificates, payments, reports, views, submissions
-
+from app.routers import auth, users, courses, enrollments, assignments, attendance, certificates, reports, views, submissions
 app = FastAPI(title="Online School API", version="2.0.0")
 
 app.include_router(auth.router)
@@ -10,7 +9,6 @@ app.include_router(enrollments.router)
 app.include_router(assignments.router)
 app.include_router(attendance.router)
 app.include_router(certificates.router)
-app.include_router(payments.router)
 app.include_router(reports.router)
 app.include_router(views.router)
 app.include_router(submissions.router)
